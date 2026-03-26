a09aa4746350fd5270372183edfef42c7a54dfa7
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-26 19:36:24 +0600

    Conditional css added and Border toggle css added

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "./country.css";
+import { useState } from "react";
 
 const Country = ({ country }) => {
-  console.log(country.area.area);
+  //   console.log(country.area.area);
+  const [visited, setVisited] = useState(false);
 
   const handleVisited = () => {
-    console.log("btn clicked");
+    // console.log("btn clicked");
+    // setVisited(true);
+
+    //! system--01
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // ! system--02
+    // setVisited(visited ? false : true);
+
+    // ! system --03
+    setVisited(!visited);
   };
 
   return (
-    <div className="country">
+    // <div className={`country ${visited ? "country-visited" : "country-non"}`}>
+    <div className={`country ${visited && "country-visited"}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Country Name: {country.name.common}</h3>
       <p>Population: {country.population.population}</p>
@@ -18,7 +34,9 @@ const Country = ({ country }) => {
         {country.area.area > 300000 ? " BIG COUNTRY" : " SMALL COUNTRY"}
       </p>
 
-      <button onClick={handleVisited}>Not Visited</button>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
